5d26f599014e431074263e6264777de9d7b135b8
NITHIN <NITHIN@LAPTOP-855T3PMD>
2021-03-15 13:54:45 +0530

    add mtd

--- GitRepo/src/test/java/com/DPT/logintest/Organization.java
+++ GitRepo/src/test/java/com/DPT/logintest/Organization.java
@@ -3,8 +3,12 @@ package com.DPT.logintest;
 import org.testng.annotations.Test;
 
 public class Organization {
-@Test
-public void org(){
-	System.out.println("organization created");
-}
+	@Test
+	public void org(){
+		System.out.println("organization created");
+	}
+	@Test
+	public void Systemdate(String date,String year){
+		System.out.println("date");
+	}
 }
