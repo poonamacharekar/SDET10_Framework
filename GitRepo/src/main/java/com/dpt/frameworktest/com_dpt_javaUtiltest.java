65c80d4d001737be687c65b45c5ee88107f19cbf
NITHIN <NITHIN@LAPTOP-855T3PMD>
2021-03-15 13:25:16 +0530

    add main

---
+++ GitRepo/src/main/java/com/dpt/frameworktest/com_dpt_javaUtiltest.java
@@ -0,0 +1,11 @@
+package com.dpt.frameworktest;
+
+import org.testng.annotations.Test;
+
+public class com_dpt_javaUtiltest {
+	@Test
+	public void randomclass(){
+		System.out.println("random class");
+	}
+
+}
