25a0722c51d9e78a94c13184214f124217342dc7
NITHIN <NITHIN@LAPTOP-855T3PMD>
2021-03-15 12:55:13 +0530

    Framework

---
+++ GitRepo/src/test/java/com/DPT/logintest/LogintoApptest.java
@@ -0,0 +1,18 @@
+package com.DPT.logintest;
+
+import org.testng.annotations.Test;
+
+public class LogintoApptest {
+	@Test
+	public void log(){
+		System.out.println("logedin");
+	}
+	@Test
+	public void logwithUN(){
+		System.out.println("done");
+	}
+	@Test
+	public void logout(){
+		System.out.println("logged out");
+	}
+}
